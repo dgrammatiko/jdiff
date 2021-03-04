a3f935d1d5a23c098517e0de5e04fd31cc5df29c
dgrammatiko <d.grammatiko@gmail.com>
2021-03-04 20:04:18 +0100

    dfsgdsfgsdfg

--- index.js
+++ index.js
@@ -5,59 +5,59 @@ const recursive = require('recursive-readdir');
 const degit = require('tiged');
 
 // Clone 3.10
-// ensureDirSync('joomla_310');
-// execSync(`git clone --depth 1 --branch 3.10-dev https://github.com/joomla/joomla-cms.git joomla_310`);
+ensureDirSync('joomla_310');
+execSync(`git clone --depth 1 --branch 3.10-dev https://github.com/joomla/joomla-cms.git joomla_310`);
 
-// const NonDeliverables = [
-//   '.git',
-//   '.github',
-//   'tests',
-//   '.appveyor.yml',
-//   '.drone.jsonnet',
-//   '.drone.yml',
-//   '.editorconfig',
-//   '.gitignore',
-//   '.php_cs',
-//   'appveyor-phpunit.xml',
-//   'CODE_OF_CONDUCT.md',
-//   'codeception.yml',
+const NonDeliverables = [
+  '.git',
+  '.github',
+  'tests',
+  '.appveyor.yml',
+  '.drone.jsonnet',
+  '.drone.yml',
+  '.editorconfig',
+  '.gitignore',
+  '.php_cs',
+  'appveyor-phpunit.xml',
+  'CODE_OF_CONDUCT.md',
+  'codeception.yml',
 
-//   'crowdin.yml',
-//   'jenkins-phpunit.xml',
-//   'Jenkinsfile',
-//   'karma.conf.js',
-//   'phpunit.xml.dist',
-//   'README.md',
-//   'RoboFile.dist.ini',
-//   'RoboFile.php',
-//   '.php_cs.dist',
-//   'build.xml',
+  'crowdin.yml',
+  'jenkins-phpunit.xml',
+  'Jenkinsfile',
+  'karma.conf.js',
+  'phpunit.xml.dist',
+  'README.md',
+  'RoboFile.dist.ini',
+  'RoboFile.php',
+  '.php_cs.dist',
+  'build.xml',
 
-//   'phpunit-pgsql.xml.dist',
-// ];
-// const NonDeliverables2 = [
-//   'build',
-//   'composer.json',
-//   'composer.lock',
-//   'package-lock.json',
-//   'package.json',
-// ];
+  'phpunit-pgsql.xml.dist',
+];
+const NonDeliverables2 = [
+  'build',
+  'composer.json',
+  'composer.lock',
+  'package-lock.json',
+  'package.json',
+];
 
 
-// NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_310/${file}`));
+NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_310/${file}`));
 
-// // Clone J4
-// ensureDirSync('joomla_400');
-// execSync(`git clone --depth 1 --branch 4.0-dev https://github.com/joomla/joomla-cms.git joomla_400`);
+// Clone J4
+ensureDirSync('joomla_400');
+execSync(`git clone --depth 1 --branch 4.0-dev https://github.com/joomla/joomla-cms.git joomla_400`);
 
-// NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_400/${file}`));
+NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_400/${file}`));
 
-// execSync(`cd joomla_400 && composer install --ignore-platform-reqs`);
-// execSync(`cd joomla_400 && npm ci`);
+execSync(`cd joomla_400 && composer install --ignore-platform-reqs`);
+execSync(`cd joomla_400 && npm ci`);
 
-// // Remove more files
-// // NonDeliverables2.map(file => removeSync(`${process.cwd()}/joomla_310/${file}`));
-// // NonDeliverables2.map(file => removeSync(`${process.cwd()}/joomla_400/${file}`));
+// Remove more files
+NonDeliverables2.map(file => removeSync(`${process.cwd()}/joomla_310/${file}`));
+NonDeliverables2.map(file => removeSync(`${process.cwd()}/joomla_400/${file}`));
 
 let J3Files;
 let J4Files;
@@ -74,5 +74,4 @@ recursive(`joomla_310`, function (err, files) {
   });
 
   writeFileSync('allFiles.json', JSON.stringify({ '310': J3Files, '40': J4Files}), ()=> {});
-
 });
