30203bf282cfce5d8ab5ce480bb3838ba3ce87c7
dgrammatiko <d.grammatiko@gmail.com>
2021-03-04 19:24:56 +0100

    memem

---
+++ index.js
@@ -0,0 +1,53 @@
+const { stat, ensureDir, remove } = require('fs-extra');
+const {execSync} = require('child_process')
+const { sep } = require('path');
+const recursive = require('recursive-readdir');
+const degit = require('tiged');
+
+// Clone 3.10
+ensureDir('joomla_310');
+execSync(`git clone --depth 1 --branch 3.10-dev https://github.com/joomla/joomla-cms.git joomla_310`);
+
+const NonDeliverables = [
+  '.git',
+  '.github',
+  // 'build',
+  'tests',
+  '.appveyor.yml',
+  '.drone.jsonnet',
+  '.drone.yml',
+  '.editorconfig',
+  '.gitignore',
+  '.php_cs',
+  'appveyor-phpunit.xml',
+  'CODE_OF_CONDUCT.md',
+  'codeception.yml',
+  'composer.json',
+  'composer.lock',
+  'crowdin.yml',
+  'jenkins-phpunit.xml',
+  'Jenkinsfile',
+  'karma.conf.js',
+  'phpunit.xml.dist',
+  'README.md',
+  'RoboFile.dist.ini',
+  'RoboFile.php',
+  '.php_cs.dist',
+  'build.xml',
+  // 'package-lock.json',
+  // 'package.json',
+  'phpunit-pgsql.xml.dist',
+
+];
+
+NonDeliverables.map(file => remove(`${process.cwd()}/joomla_310/${file}`));
+
+// Clone J4
+ensureDir('joomla_400');
+execSync(`git clone --depth 1 --branch 4.0-dev https://github.com/joomla/joomla-cms.git joomla_400`);
+
+NonDeliverables.map(file => remove(`${process.cwd()}/joomla_400/${file}`));
+
+// execSync(`cd joomla_400 && composer install --ignore-platform-reqs`);
+execSync(`cd joomla_400 && npm ci`);
+
