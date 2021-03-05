23852ef59115cd1a4feffba3bca065d1f18503e3
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2021-03-05 02:23:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ function checkFile(file) {
   }
 };
 
-// Clone 3.10
-ensureDirSync('joomla_310');
-execSync(`git clone --depth 1 --branch 3.10-dev https://github.com/joomla/joomla-cms.git joomla_310`);
 
 const NonDeliverables = [
   '.git',
@@ -44,8 +41,7 @@ const NonDeliverables = [
   'build.xml',
 
   'phpunit-pgsql.xml.dist',
-];
-const NonDeliverables2 = [
+  
   'build',
   'composer.json',
   'composer.lock',
@@ -54,33 +50,34 @@ const NonDeliverables2 = [
 ];
 
 
-NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_310/${file}`));
+// Clone 3.10
+ensureDirSync('joomla_310');
+execSync(`git clone --depth 1 --branch 3.10-dev https://github.com/joomla/joomla-cms.git joomla_310`);
 
 // Clone J4
 ensureDirSync('joomla_400');
 execSync(`git clone --depth 1 --branch 4.0-dev https://github.com/joomla/joomla-cms.git joomla_400`);
 
-NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_400/${file}`));
-
+// Build j4
 execSync(`cd joomla_400 && composer install --ignore-platform-reqs`);
 execSync(`cd joomla_400 && npm ci`);
 
-// Remove more files
-NonDeliverables2.map(file => removeSync(`${process.cwd()}/joomla_310/${file}`));
-NonDeliverables2.map(file => removeSync(`${process.cwd()}/joomla_400/${file}`));
+// Remove some non deliverable files
+NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_310/${file}`));
+NonDeliverables.map(file => removeSync(`${process.cwd()}/joomla_400/${file}`));
+
 
 let J3Files;
 let J4Files;
 
 recursive(`joomla_310`, function (err, files) {
-  // `files` is an array of file paths
-  console.log(files);
-  J3Files = files.map(file => file.replace(`joomla_310${sep}`, ''))
+//   console.log(files);
+  J3Files = files.sort().map(file => file.replace(`joomla_310${sep}`, ''))
 
   recursive(`joomla_400`, function (err, files) {
     // `files` is an array of file paths
-    console.log(files);
-    J4Files = files.map(file => file.replace(`joomla_400${sep}`, ''))
+//     console.log(files);
+    J4Files = files.sort().map(file => file.replace(`joomla_400${sep}`, ''))
     J3Files.forEach(file => checkFile(file))
 
     writeFileSync('J3Files.json', JSON.stringify(J3Files, '', 2), ()=> {});
